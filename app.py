941aac907e87c995a6a740b0c293616009e27f9e
Vaalob <jobiex@outlook.com>
2026-04-14 13:49:37 -0700

    actualizacion backend

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.cliente import cliente_bp
+import os
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(cliente_bp, url_prefix="/api")
 
+# 👇 ESTO VA SIEMPRE AL FINAL
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
